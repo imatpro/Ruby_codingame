19d2728d0761ab08191cd1faabc52d944fadcb32
Matthieu H <matthieu.hue@twenga.com>
2016-04-17 18:31:07 +0200

    APU Dinal

--- APU.rb
+++ APU.rb
@@ -4,7 +4,7 @@ STDOUT.sync = true # DO NOT REMOVE
 @width = gets.to_i # the number of cells on the X axis
 @height = gets.to_i # the number of cells on the Y axis
 @tableau=[]
-   
+
 @height.times do
     line = gets.chomp # width characters, each either 0 or .
     line = line.split(//)
@@ -12,53 +12,45 @@ STDOUT.sync = true # DO NOT REMOVE
 end
 
 
-
 class Node
-    def initialize(x1, y1, tab, ligne, colonne)
+    def initialize(x1, y1, tab, colonne, ligne)
         #attr_accessor :x1, :y1, :tab, :ligne, :colonne 
         @x1=x1.to_i
         @y1=y1.to_i
         @tab=tab
         @ligne=ligne.to_i
-        @colonne=colonne.to_i
-        
+        @colonne=colonne.to_i    
     end
     
     def check_right
-        if @y1 < @ligne-1
-            if @tab[@x1][@y1+1] == "0"
-                n = "1 0"
-            else n = "-1 -1"
+        n = "-1 -1"
+        if @y1 < @colonne-1
+            nb=@y1+1
+            while @tab[@x1][nb] != "0" && nb < @colonne-1 do 
+                nb+=1
             end
-        else n = "-1 -1"
+            n = "#{nb} #{@x1}" if @tab[@x1][nb] == "0"
         end
         return n
     end
     
     def check_bottom
-        if @x1 < @colonne-1
-            if @tab[@x1+1][@y1] == "0"
-                n = "0 1"
-            else n = "-1 -1"
+        n = "-1 -1"
+        if @x1 < @ligne-1
+            nb=@x1+1
+            while @tab[nb][@y1] != "0" && nb < @ligne-1 do 
+                nb+=1
             end
-        else n = "-1 -1"
+            n = "#{@y1} #{nb}" if @tab[nb][@y1] == "0"
         end
         return n
     end
 end
 
-
- 
-
-@tableau.each_with_index do |x,xi|
-   #  x.to_s
-     #STDERR.puts x.length
-    # for i in 0...x.length
-    # STDERR.print tab[xi][i]
-    x.each_with_index do |y, yi|  
-        
-        if @tableau[xi][yi] == "0"
-            node = Node.new(xi,yi, @tableau, @width, @height)
+@tableau.each_with_index do |y,yi|
+    y.each_with_index do |x, xi|
+        if @tableau[yi][xi] == "0"
+            node = Node.new(yi,xi, @tableau, @width, @height)
             print "#{xi} #{yi} #{node.check_right} #{node.check_bottom}\n"
         end
     end
